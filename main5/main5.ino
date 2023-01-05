02d6227aea2c38059612a300f8be3ce9e4a9a77b
Koshikawaxxx1927 <ebi1927@icloud.com>
2023-01-05 13:00:32 +0900

    Try

--- main5/main5.ino
+++ main5/main5.ino
@@ -59,57 +59,57 @@ void run() {
 void ahead_run() {
   // constexpr uint8_t right_left_difference = 25;
   switch (state) {
-    case STATE::DANGER : forward(10, 10); break;
-    default : forward(200, 200); break;
+    case STATE::DANGER : forward(50, 50); break;
+    default : forward(100, 100); break;
   }
 }
 // 右方へ走行
 void right_run() {
   // constexpr uint8_t right_left_difference = 50;
   switch (state) {
-    case STATE::DANGER : forward(10, 0); break;
-    default : forward(200, 10); break;
+    case STATE::DANGER : forward(70, 0); break;
+    default : forward(80, 10); break;
   }
 }
 // 左方へ走行
 void left_run() {
   // constexpr uint8_t right_left_difference = 50;
   switch (state) {
-    case STATE::DANGER : forward(0, 10); break;
-    default : forward(10, 200); break;
+    case STATE::DANGER : forward(0, 70); break;
+    default : forward(10, 80); break;
   }
 }
 
-// // 進行方向(前方車の方向)を決定
+// 進行方向(前方車の方向)を決定
+void decide_target() {
+  if (centimeter.center < distance::far &&
+      centimeter.right < distance::far &&
+      centimeter.left < distance::far) {
+    target = TARGET::AHEAD;
+  } else if (centimeter.right < distance::far) {
+    target = TARGET::RIGHT;
+  } else if (centimeter.left < distance::far) {
+    target = TARGET::LEFT;
+  } else if (centimeter.center < distance::far) {
+    target = TARGET::AHEAD;
+  }
+}
+
 // void decide_target() {
-//   if (centimeter.center < distance::far &&
-//       centimeter.right < distance::far &&
-//       centimeter.left < distance::far) {
+//   uint16_t min_distance = distance::far;
+//   if (centimeter.center < min_distance) {
 //     target = TARGET::AHEAD;
-//   } else if (centimeter.right < distance::far) {
+//     min_distance = centimeter.center;
+//   }
+//   if (centimeter.right < min_distance) {
 //     target = TARGET::RIGHT;
-//   } else if (centimeter.left < distance::far) {
+//     min_distance = centimeter.right;
+//   }
+//   if (centimeter.left < min_distance) {
 //     target = TARGET::LEFT;
-//   } else if (centimeter.center < distance::far) {
-//     target = TARGET::AHEAD;
+//     min_distance = centimeter.left;
 //   }
 // }
-
-void decide_target() {
-  uint16_t min_distance = distance::far;
-  if (centimeter.center < min_distance) {
-    target = TARGET::AHEAD;
-    min_distance = centimeter.center;
-  }
-  if (centimeter.right < min_distance) {
-    target = TARGET::RIGHT;
-    min_distance = centimeter.right;
-  }
-  if (centimeter.left < min_distance) {
-    target = TARGET::LEFT;
-    min_distance = centimeter.left;
-  }
-}
 
 // 前方車までの距離の状態を決定
 void decide_state() {
